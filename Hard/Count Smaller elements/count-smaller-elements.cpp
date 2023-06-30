500f5cb1ec05ba020887481458738a99a50e60a0
Shubham Srivastava <87631133+Shubham1727@users.noreply.github.com>
2023-07-01 00:52:55 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Count Smaller elements/count-smaller-elements.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> constructLowerArray(int *arr, int n) {
+	    // code here
+	          vector<int> ans, temp;
+        for (int i = n - 1; i >= 0; i--) {
+            int c = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            ans.push_back(c);
+            temp.insert(temp.begin() + c, arr[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.constructLowerArray(arr, n);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
